171bddaaab93a16279d9741730e5977185e18fed
Kanishka S <kanishka.s@mksvision.com>
2025-02-27 12:22:08 +0530

    BASE URL change Commit

--- frontend/src/apis/axiosInstance.js
+++ frontend/src/apis/axiosInstance.js
@@ -8,8 +8,7 @@ import {
   removeTokenCookie,
 } from "../helper/auth";
 
-const API_BASE_URL =
-  "wing-span-zxbn-dq7b2ef02-kanishka2727-gmailcoms-projects.vercel.app/api";
+const API_BASE_URL = "https://wing-span-zxbn.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
